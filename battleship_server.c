2d67bbb34bdb539646872ac3bd810c3efac43607
lazered <60900690+lazered@users.noreply.github.com>
2020-02-27 20:46:28 -0500

    Update battleship_server.c

--- battleship_server.c
+++ battleship_server.c
@@ -1,6 +1,6 @@
 //Code by Brandon Barrantes for CSI 333 Programming at the Hardware/Software Interface
 //FOR SERVER: Compile and input '7894' as the command line argument
-//FOR CLIENT: Compile and input the IP that comes up on when you run the server and then the same port number '7894'
+//FOR CLIENT: Compile and input the IP that comes up when you run the server and then the same port number '7894'
 
 #define _GNU_SOURCE
 
